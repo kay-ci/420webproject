9e43da9f82f7f09b8da923f644078c1f0e79087d
Jimmy <jimmy.xu@dawsoncollege.qc.ca>
2023-04-07 14:47:16 -0400

    wrote competency class

---
+++ ProjectApp/competencies/competency.py
@@ -0,0 +1,22 @@
+class Competency:
+    def __init__(self, id, competency, competency_achievement, competency_type):
+        if not isinstance(id, str):
+            raise TypeError("id must be a string")
+        if len(str(id)) > 4:
+            raise ValueError("id must not exceed 4 characters")
+        if not isinstance(competency, str):
+            raise TypeError("competency must be a string")
+        if len(str(competency)) > 250:
+            raise ValueError("competency must not exceed 250 characters")
+        if not isinstance(competency_achievement, str):
+            raise TypeError("competency achievement must be a string")
+        if len(str(competency_achievement)) > 500:
+            raise ValueError("competency achievement must not exceed 500 characters")
+        if not isinstance(competency_type, str):
+            raise TypeError("competency type must be a string")
+        if len(str(competency_type)) > 10:
+            raise ValueError("competency type must not exceed 10 characters")
+        self.id = id
+        self.competency = competency
+        self.competency_achievement = competency_achievement
+        self.competency_type = competency_type

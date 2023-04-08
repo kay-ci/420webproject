5b979bd276560f85f1d8b716fa5e2b16f598466f
Jimmy <jimmy.xu@dawsoncollege.qc.ca>
2023-04-08 19:19:21 -0400

    refactored code and added flashing messages when inserting a new competency

--- ProjectApp/competencies/competency_views.py
+++ ProjectApp/competencies/competency_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .competency import Competency, CompetencyForm
 from ..dbmanager import get_db
 
@@ -7,14 +7,13 @@ bp = Blueprint("competency", __name__, url_prefix="/competencies")
 @bp.route("/", methods = ["GET", "POST"])
 def list_competencies():
     form = CompetencyForm()
-    competencies = get_db().get_competencies()
     if request.method == "POST" and form.validate_on_submit():
         newCompetency = Competency(form.id.data, form.competency.data, form.competency_achievement.data, form.competency_type.data)
         try:
-            get_db().get_competency(form.id.data)
-        except ValueError:
-            get_db().add_competency(newCompetency)
-    return render_template("competencies.html", competencies = competencies, form = form)
+            get_db().add_competency(newCompetency)#will throw on duplicate
+        except ValueError as e:
+            flash(e)
+    return render_template("competencies.html", competencies = get_db().get_competencies(), form = form)
 
 @bp.route("/delete/<id>")
 def delete_competency(id):

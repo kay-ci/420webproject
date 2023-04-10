e1b4f2ff309f3c490440b0feda3fc0f667d3a47b
Kayci Nicole Davila <kayci.davila@dawsoncollege.qc.ca>
2023-04-10 18:08:14 -0400

    Added headers for the methods to implement - course element

---
+++ ProjectApp/courses/courses_element.py
@@ -0,0 +1,17 @@
+class CourseElement:
+    def __init__ (self):
+        pass
+    def __repr__(self):
+        pass
+    def __str__(self):
+        pass
+    def to_json():
+        pass
+    def from_json():
+        pass
+    
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import data_required
+class CourseElementForm:
+    pass

bf4507a99979a9d577a77ce6e542bbbaceea02ff
Jimmy <jimmy.xu@dawsoncollege.qc.ca>
2023-04-10 19:11:59 -0400

    added route for showing 1 competency, added try catch for deleting an inexistant competency, renamed list_competencies to show_competencies

--- ProjectApp/competencies/competency_views.py
+++ ProjectApp/competencies/competency_views.py
@@ -1,11 +1,11 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, abort
 from .competency import Competency, CompetencyForm
 from ..dbmanager import get_db
 
 bp = Blueprint("competency", __name__, url_prefix="/competencies")
 
 @bp.route("/", methods = ["GET", "POST"])
-def list_competencies():
+def show_competencies():
     form = CompetencyForm()
     if request.method == "POST" and form.validate_on_submit():
         newCompetency = Competency(form.id.data, form.competency.data, form.competency_achievement.data, form.competency_type.data)
@@ -15,7 +15,20 @@ def list_competencies():
             flash(e)
     return render_template("competencies.html", competencies = get_db().get_competencies(), form = form)
 
-@bp.route("/delete/<id>")
+@bp.route("/delete/<id>/")
 def delete_competency(id):
-    get_db().delete_competency(id)
-    return redirect(url_for("competency.list_competencies"))
+    try:
+        get_db().delete_competency(id)
+    except ValueError:
+        flash("couldn't find competency with this id to delete")
+    return redirect(url_for("competency.show_competencies"))
+
+@bp.route("/<id>/")
+def show_competency(id):
+    if not isinstance(id, str):
+        flash("could not find a competency with this id")
+    competency = get_db().get_competency(id)
+    if competency == None:
+        flash("could not find a competency with this id")
+        return redirect(url_for('show_competencies')), 404
+    return render_template("competency.html", competency = competency)

cac244866a84847f0975ce2dc3edf88de32d10ae
Kayci Nicole Davila <kayci.davila@dawsoncollege.qc.ca>
2023-04-10 19:56:44 -0400

    Made course element object and its form

--- ProjectApp/courses/courses_element.py
+++ ProjectApp/courses/courses_element.py
@@ -1,17 +1,24 @@
 class CourseElement:
-    def __init__ (self):
-        pass
+    def __init__ (self, course_id, element_id, hours):
+        if not isinstance (course_id, str):
+            raise Exception ("course id is not a str")
+        if not isinstance (element_id, str):
+            raise Exception ("Element id is not str")
+        if not isinstance (hours, int) or not isinstance(hours, float):
+            raise Exception ("hours is not int or float")
     def __repr__(self):
-        pass
+        return f'CourseElement({self.course_id}, {self.element_id}, {self.hours})'
     def __str__(self):
-        pass
+        return f'{self.course_id} {self.element_id} {self.hours}'
     def to_json():
         pass
     def from_json():
         pass
     
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import data_required
+from wtforms import StringField, FloatField
+from wtforms.validators import DataRequired
 class CourseElementForm:
-    pass
+    course_id = StringField('Course ID', [DataRequired()])
+    element_id = StringField('Element ID', [DataRequired()])
+    hours = FloatField('Hours', [DataRequired()])

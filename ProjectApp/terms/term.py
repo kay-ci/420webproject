3cf197952f344794c7ae40458b5f7a35798480b1
Jimmy <jimmy.xu@dawsoncollege.qc.ca>
2023-04-10 20:31:10 -0400

    added repr and str for term

--- ProjectApp/terms/term.py
+++ ProjectApp/terms/term.py
@@ -12,3 +12,7 @@ class term:
             raise ValueError("name can not be null")
         self.id = id
         self.name = name
+    def __repr__(self):
+        return f"{self.id}({self.name})"
+    def __str__(self):
+        return f"{self.id}: {self.name}"

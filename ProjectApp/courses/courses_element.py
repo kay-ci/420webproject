78925336a82a6ccd53d2171600e736f450674adb
Jimmy <jimmy.xu@dawsoncollege.qc.ca>
2023-04-16 13:00:39 -0400

    fixed mistake in CourseElement constructor expecting a string element id

--- ProjectApp/courses/courses_element.py
+++ ProjectApp/courses/courses_element.py
@@ -2,8 +2,8 @@ class CourseElement:
     def __init__ (self, course_id, element_id, hours):
         if not isinstance (course_id, str):
             raise Exception ("course id is not a str")
-        if not isinstance (element_id, str):
-            raise Exception ("Element id is not str")
+        if not isinstance (element_id, int):
+            raise Exception ("Element id is not an int")
         if not isinstance(hours, float):
             raise Exception ("hours is not int or float")
         self.course_id = course_id

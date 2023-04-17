5c1546dcf67e1efcf258d070b31e9b2290dfa6ee
Jimmy <jimmy.xu@dawsoncollege.qc.ca>
2023-04-16 21:57:37 -0400

    added form for creating and adding an element to a competency and handled a route for this

--- ProjectApp/competencies/competency_views.py
+++ ProjectApp/competencies/competency_views.py
@@ -35,17 +35,22 @@ def delete_competency(id):
 
 @bp.route("/<id>/", methods = ["GET", "POST"])
 def show_competency(id):
-    form = CompetencyForm()
+    competency_form = CompetencyForm()
+    element_form = ElementForm()
     if not isinstance(id, str):
         flash("could not find a competency with this id")
     competency = get_db().get_competency(id)
     if competency == None:
         flash("could not find a competency with this id")
         return redirect(url_for('competency.show_competencies')), 404
-    if request.method == "POST" and form.validate_on_submit():
-        get_db().update_competency(id, form.id.data, form.competency.data, form.competency_achievement.data, form.competency_type.data)
-        competency = get_db().get_competency(form.id.data)
-        return redirect(url_for("competency.show_competency", id = form.id.data))
+    if request.method == "POST":
+        if element_form.element_order.data == None:#if true then its the competency form that was submitted
+            competency_form.validate_on_submit()#what does validate_on_submit even do?
+            get_db().update_competency(competency_form.competency_id.data, competency_form.competency.data, competency_form.competency_achievement.data, competency_form.competency_type.data)
+        else:# means the submitted form is element
+            element = Element(None, element_form.element_order.data, element_form.element.data, element_form.element_criteria.data, id)
+            get_db().add_element(element)
+        return redirect(url_for("competency.show_competency", id = id))
     achievements = competency.competency_achievement.split("*")
     achievements.pop(0)
-    return render_template("competency.html", competency = competency, form = form, achievements = achievements, elements = get_db().get_competency_elements(id))
+    return render_template("competency.html", competency = competency, competency_form = competency_form, element_form = element_form, achievements = achievements, elements = get_db().get_competency_elements(id))

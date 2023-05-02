85f3daf3b2e8fbdbab5f86c31b050b092dda4f84
Jimmy <jimmy.xu@dawsoncollege.qc.ca>
2023-05-02 17:50:42 -0400

    made list_courses_elements render its html regardless of the http method

--- ProjectApp/courses/courses_elements_views.py
+++ ProjectApp/courses/courses_elements_views.py
@@ -13,8 +13,7 @@ def list_courses_elements():
             get_db().add_courses_element(new_course_element)
         except ValueError as e:
             flash(e)
-    if request.method == "GET":
-        return render_template("courses_elements.html", courses_elements = get_db().get_courses_elements(), form = form)
+    return render_template("courses_elements.html", courses_elements = get_db().get_courses_elements(), form = form)
     
 #display all elements for a given course
 @bp.route("course/<element_id>/")

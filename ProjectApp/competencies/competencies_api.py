c19f9b6831dc76f5898c8049effff91a032d488b
Jimmy <jimmy.xu@dawsoncollege.qc.ca>
2023-05-04 22:31:42 -0400

    completed all required methods for api with their unit test

--- ProjectApp/competencies/competencies_api.py
+++ ProjectApp/competencies/competencies_api.py
@@ -46,6 +46,8 @@ def competencies_api():
 
 @bp.route('/<competency_id>', methods = ["GET", "PUT", "DELETE"])
 def competency_api(competency_id):
+    if len(competency_id) > 4:
+        return make_response({"description":"competency id has a max length of 4 characters"}, 404)
     if request.method == "DELETE":
         try:
             get_db().delete_competency(competency_id)
@@ -87,14 +89,18 @@ def competency_api(competency_id):
                     if competency_json["competency_type"] != "Mandatory" and competency_json["competency_type"] != "Optional":
                         return make_response({"description":"competency_type has to be 'Mandatory' or 'Optional'"}, 400)
                     competency.competency_type = competency_json["competency_type"]
-                get_db().update_competency(competency)#if id already used, will return 400 with str(e)
+                competency = Competency.from_json_without_id(competency_json, competency_id)
+                get_db().update_competency(competency.id, competency.competency, competency.competency_achievement, competency.competency_type)#if id already used, will return 400 with str(e)
                 resp = make_response({}, 201)
                 resp.headers["Location"] = url_for("competencies_api.competency_api", competency_id = competency_id)
                 return resp
             except ValueError as e:
                 return make_response({"description":str(e)}, 400)
     elif request.method == "GET":
-        pass
+        if get_db().get_competency(competency_id) == None:
+            return make_response({"description":"could not find competency for this id"}, 404)
+        json = get_db().get_competency(competency_id).__dict__
+        return make_response(json, 200)
      
 @bp.route('/<competency_id>/', methods=["GET","POST"])
 def competency_elements_api():
--- Tests/test_competencies_api.py
+++ Tests/test_competencies_api.py
@@ -12,6 +12,7 @@ class TestCompetencyAPI(flask_unittest.ClientTestCase):
         self.assertIsNone(json['prev_page'])
 
     def test_post_and_delete_competency(self, client):
+        #setup code (adds a test competency to db)
         competency_with_elements = {"id":"test", "competency":"test-name", "competency_achievement":"test-achievement", "competency_type":"Mandatory"}
         elements = []
         elements.append({"element":"test-name", "element_criteria":"test criteria"})
@@ -36,6 +37,36 @@ class TestCompetencyAPI(flask_unittest.ClientTestCase):
         competency_with_elements["competency"] = "updated name"
         resp = client.put('api/competencies/test', json = competency_with_elements)
         self.assertEqual(resp.status_code, 201)
+
+        #cleanup code (removes test competency from db)
+        resp = client.delete('api/competencies/test')
+        self.assertEqual(resp.status_code, 204)
+
+    def test_put_new_competency(self, client):
+        competency_with_elements = {"id":"test", "competency":"test-name", "competency_achievement":"test-achievement", "competency_type":"Mandatory"}
+        elements = []
+        elements.append({"element":"test-name", "element_criteria":"test criteria"})
+        elements.append({"element":"test-name2", "element_criteria":"test criteria2"})
+        competency_with_elements["elements"] = elements
+        resp = client.put('api/competencies/test', json = competency_with_elements)
+        self.assertEqual(resp.status_code, 201)
+
+        #cleanup code (removes test competency from db)
+        resp = client.delete('api/competencies/test')
+        self.assertEqual(resp.status_code, 204)
+
+    def test_get_competency(self, client):
+        #setup code (adds a test competency to db)
+        competency_with_elements = {"id":"test", "competency":"test-name", "competency_achievement":"test-achievement", "competency_type":"Mandatory"}
+        elements = []
+        elements.append({"element":"test-name", "element_criteria":"test criteria"})
+        elements.append({"element":"test-name2", "element_criteria":"test criteria2"})
+        competency_with_elements["elements"] = elements
+        resp = client.post('/api/competencies/', json=competency_with_elements)
+        self.assertEqual(resp.status_code, 201)
+
+        resp = client.get('api/competencies/test')
+        self.assertEqual(resp.status_code, 200)
 
         #cleanup code (removes test competency from db)
         resp = client.delete('api/competencies/test')
